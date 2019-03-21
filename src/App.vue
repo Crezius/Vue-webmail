01a95f0b3401dfbf0196c22d9fff2c9330d80044
Crezius <guignard.arnaud@orange.fr>
2019-03-21 22:37:49 +0100

    second commit

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,69 @@
 <style scoped src="@/assets/css/style.css">
 </style>
 
-
 <template>
-    <div :style="{backgroundColor: currentColor}">
-        <p> Mon Stroboscope</p>
-        <button @click="strobos()" style="text-align: center">Start / Stop</button> 
-        <button @click="reset()" style="text-align: center">Reset</button> 
-    </div>
+
+    <body>
+    
+    <!--
+        <div class="block header">
+            <h1>Webmail</h1>
+            <div>
+                <input type="texte" class="input-text" value="coucou">
+            <div>
+        </div>
+        
+        <div class="block ajout">
+                <input type="texte" value="coucou">
+        </div>
+    <!--
+        <div class="strobo" :style="{backgroundColor: currentColor}">
+
+            
+            <p> Mon Stroboscope</p>
+            <button @click="strobos()" style="text-align: center">Start / Stop</button> 
+            <button @click="reset()" style="text-align: center">Reset</button> 
+        </div>
+        
+    -->
+    
+    <div id="Connexion" class="block">
+            <h1 id="id_user">Caramail 2 </h1>
+
+            <form id="form_connexion" onsubmit="return connexion('')" method="get">
+				<input type="text" name="user" maxlength="20"/>
+				<input type="submit" value="Connexion">
+			</form>
+        </div>
+		
+		<div id="creation_mail" class="block">
+			<form id="envoi">
+				<div id="_Destinataire" class="block">
+					<label for="dest">Destinataire:  </label>
+					<input type="text" id="dest" name="dest" style="width:100%" maxlength="20"/>
+				</div>
+				<div id="_Message" class="block">
+					<label for="message">Message:  </label>
+					<input type="text" id="message" name="message"  style="width:100%" maxlength="300"/>
+					<input type="submit" value="Envoyer"  id="btnEnvoyer">
+				</div>
+			</form>
+        </div>
+		
+		
+
+		<div id="gauche" class="block">
+            <input type="submit" value="FLETCH" id="fletch">
+
+			<ul id="_liste_mail">
+				
+			</ul>
+		</div>
+		<div id="droite" class="block">
+		  Coucou
+		</div>
+    
+    </body>
 </template>
 
 
@@ -67,7 +123,20 @@
                 this.stop = true;
                 clearInterval(this.timer);
                 this.currentColor = `white`
-            }
+            },
+            
+             supprimer(id, id_liste){
+
+                xhr = new XMLHttpRequest();
+				xhr.open('DELETE', 'supprimer.php?idSUP=' + id);
+				xhr.send(null);
+
+                
+                var list_mail = document.getElementById("_liste_mail");
+                list_mail.removeChild(document.getElementById("liste_mail"+id_liste));
+                
+                return false;
+			}
         }
         
     };
